1e670b272d8e7b4b44816c864672eeca16a696f1
Jorge Garcia <geor.1442@gmail.com>
2025-10-06 23:15:08 -0500

    Agregando cambios a la capa DTO

--- ApiParking/src/main/java/co/edu/uco/parking/dto/CellTypeDTO.java
+++ ApiParking/src/main/java/co/edu/uco/parking/dto/CellTypeDTO.java
@@ -1,5 +1,58 @@
 package co.edu.uco.parking.dto;
 
-public class CellTypeDTO {
+import java.util.UUID;
+
+public final class CellTypeDTO {
+	
+	private UUID id;
+	private String name;
+	private String description;
+	private boolean active;
+	
+	public CellTypeDTO() {
+		this.id = null;
+		this.name = "";
+		this.description = "";
+		this.active = false;
+	}
+	
+	public CellTypeDTO(final UUID id, final String name, final String description, final boolean active) {
+		this.id = id;
+		this.name = name;
+		this.description = description;
+		this.active = active;
+	}
+	
+	public UUID getId() {
+		return id;
+	}
+	
+	public void setId(UUID id) {
+		this.id = id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+	
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	public boolean isActive() {
+		return active;
+	}
+	
+	public void setActive(boolean active) {
+		this.active = active;
+	}
 
 }
